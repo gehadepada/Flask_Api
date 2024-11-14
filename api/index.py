baec17a9eed3a0b319080449822a1389c54cbed2
gehadepada <gehadepada@gmail.com>
2024-11-14 23:37:32 +0200

    index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Load the trained model
+model = joblib.load('/content/drive/My Drive/linear_regression_model.pkl')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the data from the POST request
+    data = request.get_json()
+    # Convert the data into a DataFrame
+    df = pd.DataFrame(data)
+    # Make prediction
+    prediction = model.predict(df)
+    # Return the prediction as a JSON response
+    return jsonify(prediction.tolist())
+
+if _name_ == '_main_':
+    app.run(debug=True)
