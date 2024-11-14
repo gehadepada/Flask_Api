2324112c6c0d82c0e86679890288b7d945c1e894
gehadepada <gehadepada@gmail.com>
2024-11-15 00:43:09 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the data
-data = pd.read_excel('C:/Users/VIP/Desktop/patients.xlsx')
+data = pd.read_excel('patients.xlsx')
 
 # Define features (input variables) and labels (output variable)
 features = data.iloc[:, :-1].values
