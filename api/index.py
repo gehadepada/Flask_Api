660f79df71eaaab7361e9f1407e59b6016c01396
gehadepada <gehadepada@gmail.com>
2024-11-15 00:20:55 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import gdown
 
-app = Flask(_name_)
+app = Flask(__name__)
+
+# Google Drive file ID
+file_id = '1yY2fmAuuzfelOZpJkMGzIL1PRLKiu6rs'
+url = f'https://drive.google.com/uc?id={file_id}'
+
+# Download the model file
+output = 'linear_regression_model.pkl'
+gdown.download(url, output, quiet=False)
 
 # Load the trained model
-model = joblib.load('/content/drive/My Drive/linear_regression_model.pkl')
+model = joblib.load(output)
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -18,5 +27,6 @@ def predict():
     # Return the prediction as a JSON response
     return jsonify(prediction.tolist())
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
+
